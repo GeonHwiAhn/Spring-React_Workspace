326272b80d32052d0cc93b8092c65266843bb67e
GeonHwiAhn <169108642+GeonHwiAhn@users.noreply.github.com>
2024-07-29 15:29:52 +0900

    naver login

---
+++ naver-login/src/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Login = () => {
+    return (
+        <>
+        <a href="http://localhost:9010/naverLogin">
+            <img height="50" src="http://static.nid.naver.com/oauth/small_g_in.PNG" />
+            네이버로 로그인하기
+        </a>
+        </>
+    )
+}
+
+export default Login;
