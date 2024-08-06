d53580110d7e9d6cd4593ce7605490f69bf431f3
GeonHwiAhn <169108642+GeonHwiAhn@users.noreply.github.com>
2024-08-06 17:48:07 +0900

    .

--- TodoList-Backend/src/main/java/todo/mapper/TodoMapper.java
+++ TodoList-Backend/src/main/java/todo/mapper/TodoMapper.java
@@ -12,10 +12,11 @@ public interface TodoMapper {
 	int idCheck(String id);
 	int signup(TodoMember member);
 	TodoMember login(TodoMember member);
+	//로그인 위한 정보 : id/pw 
+	//로그인 성공시 가져오는 정보 : id/pw 에 해당되는 유저 정보 (SELECT 결과)
 	List<Todo> selectTodoList(int todoMemberNo);
 	int insert(Todo todo);
 	int update(Todo todo);
 	int delete(int todoNo);
-	
 	
 }
