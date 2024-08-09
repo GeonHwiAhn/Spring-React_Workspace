185cb699a0035682fd784e8d11af99844ff51ecc
GeonHwiAhn <169108642+GeonHwiAhn@users.noreply.github.com>
2024-08-09 16:33:29 +0900

    이미지업로드페이지

--- image-front/src/component/layout/Header.js
+++ image-front/src/component/layout/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return(
         <nav className="header-nav">
             <ul className="nav-list">
-                <li className="nav-item"><Link to="/" className="nav-link">메인페이지</Link></li>
+                <li className="nav-item"><Link to="/board" className="nav-link">메인페이지</Link></li>
                 <li className="nav-item"><Link to="/board" className="nav-link">게시판</Link></li>
                 <li className="nav-item"><Link to="/profile" className="nav-link">프로필</Link></li>
             </ul>
