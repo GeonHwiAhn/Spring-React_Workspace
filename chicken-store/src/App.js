43ce0fcf7d3e83826abb6d8aecf8ecea5329fcfc
GeonHwiAhn <169108642+GeonHwiAhn@users.noreply.github.com>
2024-08-21 11:04:35 +0900

    JPA

--- chicken-store/src/App.js
+++ chicken-store/src/App.js
@@ -1,32 +1,33 @@
-import './App.css';
-import ChickenForm from './component/ChickenForm';
-import ChickenList from './component/ChickenList';
-import Modal from './component/Modal';
-import {useState} from 'react';
-
-const App= () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  //사용자가 보고싶을 때 볼 수 있도록 처음에는 false 보이지않음 설정해줌
+import "./App.css";
+import ChickenList from "./component/ChickenList";
+import ChickenForm from "./component/ChickenForm";
+import Modal from "./component/Modal";
+import { useState } from "react";
+const App = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false); 
+  //사용자가 보고싶을 때 볼 수 있도록 처음에는 false 보이지않음 설정 해줌
 
   // 오픈 true 닫음 false
-  // const에서 동작하는 기능이 1개일 때 {} 생략 가능
+  // const에서 동작하는 기능이 1개일 때 {} 중괄호 생략 가능
   const openModal = () => setIsModalOpen(true);
-  
+
   const closeModal = () => {
     setIsModalOpen(false);
   }
 
-
   return (
-    <div className="App">
+    <div className="app-container">
+      <h1>치킨 가게 메뉴 관리</h1>
+      <button onClick={openModal}>메뉴등록하기</button>
       <ChickenList />
 
-      <button onClick={openModal}>메뉴등록하기</button>
+
+    
       <Modal isOpen={isModalOpen} onClose={closeModal}>
-        <ChickenForm />
+          <ChickenForm />
       </Modal>
     </div>
   );
-}
+};
 
 export default App;
